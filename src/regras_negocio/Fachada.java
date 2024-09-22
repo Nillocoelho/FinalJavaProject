189152d7d3ecce81f86de1c1b31006bc9924dc63
Danillo Coelho <coelho.danillo@academico.ifpb.edu.br>
2024-09-21 23:44:39 -0300

    Grandes mudanças

--- src/regras_negocio/Fachada.java
+++ src/regras_negocio/Fachada.java
@@ -12,17 +12,31 @@ public class Fachada {
 	public static ArrayList<Correntista> Correntista = new ArrayList<Correntista>();
 	public static ArrayList<Conta> Conta = new ArrayList<Conta>();
 	public static ArrayList<Correntista> listarCorrentistas(){
-		return repositorio.getCorrentista();
+		return repositorio.getCorrentistas();
 	}
+    public static ArrayList<Correntista> listarCorrentistas(String cpf) {
+        ArrayList<Correntista> listaCor = new ArrayList<>();
+        for(Correntista co: repositorio.getCorrentistas())
+            if(co.getCpf().equals(cpf))
+                listaCor.add(co);
+        return listaCor;
+    }
 	public static ArrayList<Conta> listarContas(){
-		return repositorio.getConta();
+		return repositorio.getContas();
 	}
+    public static ArrayList<Conta> listarContas(int id) {
+        ArrayList<Conta> lista = new ArrayList<>();
+        for(Conta c: repositorio.getContas())
+            if(c.getId() == id)
+                lista.add(c);
+        return lista;
+    }
 	public static void criarCorrentista(String cpf,String nome,String senha)throws Exception{
 		cpf = cpf.trim();
 		nome = nome.trim();
-		Correntista co = repositorio.listarCorrentistas(cpf,nome);
-		for(Correntista correntista : getCorrentistas()) {
-			if(correntista.getcpf(cpf).equals(cpf))
+		Correntista co = repositorio.localizarCorrentista(cpf);
+		for(Correntista correntista : listarCorrentistas()) {
+			if(correntista.getCpf().equals(cpf))
 				throw new Exception("Erro, CPF já se encontra no cadastro de correntista");
 				return;
 		}
@@ -30,19 +44,18 @@ public class Fachada {
 			throw new Exception("A senha deve ter 4 números");
 		 co = new Correntista(cpf, nome, senha);
 		 
-		 repositorio.adicionar(co);
+		 repositorio.adicionarCorrentista(co);
 		 repositorio.salvarObjetos();
 	}
-	public static void criarConta(int id){
-		Correntista co = repositorio.listarContas(id);
-		for(Conta conta : getConta()) {
-			if(conta.getcpf(id).equals(id)) {
+	public static void criarConta(int id)throws Exception{
+		Conta c = repositorio.localizarConta(id);
+		for(Conta conta : listarContas()) {
+			if(conta.getId() == id) {
 				throw new Exception("Erro, já existe uma conta vinculada a esse CPF");
-				return;
 			}
-			c = new Conta(id);
-			
 		}
+		 repositorio.adicionarConta(c);
+		 repositorio.salvarObjetos();
 	}
 	public static void criarContaEspecial(String cpf, double limite){
 		
@@ -60,12 +73,13 @@ public class Fachada {
 		
 	}
 	public static void debitarValor(int id, String cpf, String senha, double valor)throws Exception{
-		if (Correntista.saldo < valor) {
+		if (Conta.GetSaldo() < valor) {
 			throw new Exception("Conta com saldo insuficiente para realizar a transação");
 		}
 	}
-	public static void transferirValor(int id1, String cpf, String senha, double valor, int id2){
-		
+	public static void transferirValor(int id, String cpf, String senha, double valor, int id2) throws Exception{
+		for 
+			throw new Exception("Conta não encontrada.");
 	}
 
 
