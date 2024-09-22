1435882e9399bd9bfbe962ae4c8c3b57abdda4e8
Danillo Coelho Barbosa <coelho.danillo@academico.ifpb.edu.br>
2024-09-22 00:14:07 -0300

    Update ContaEspecial.java

--- src/modelo/ContaEspecial.java
+++ src/modelo/ContaEspecial.java
@@ -2,45 +2,19 @@ package modelo;
 
 public class ContaEspecial extends Conta{
 	private double limite;
-<<<<<<< HEAD
-	
-	public ContaEspecial(int id, String data, double saldo, double limite) {
-		super(id, data, saldo);
-=======
 	public ContaEspecial(int id, String data, double saldo, double limite){
 		super(id,data,saldo);
->>>>>>> bca41922c7f6feae3299f80fd4e38bb52f845c21
 		this.limite = limite;
 	}
-
 	public void debitar(double valor) {
-<<<<<<< HEAD
-        if (getSaldo() + limite < valor) {
-            throw new IllegalArgumentException("Saldo insuficiente, incluindo o limite.");
-        }
-        setSaldo(getSaldo() - valor); // Debita o valor da conta
-    }
+		this.limite = limite - valor;
+	}
 	public double getLimite() {
-        return this.limite;
-    }
-	public void setLimite(double limite) {
-        if (limite < 50) {
-            throw new IllegalArgumentException("O limite deve ser maior ou igual a 50 reais.");
-        }
-        this.limite = limite;
-    }
+		return limite;
+	}
 	@Override
 	public String toString() {
-	    return "Conta Especial {" +
-	            "ID: " + getId() + 
-	            ", Data: " + getData() +
-	            ", Saldo: " + getSaldo() +
-	            ", Limite: " + limite +
-	            '}';
-=======
-		this.limite = limite - valor;
->>>>>>> bca41922c7f6feae3299f80fd4e38bb52f845c21
+		return "ContaEspecial [limite=" + limite + "]";
 	}
-		
 	
 }
